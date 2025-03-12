7146cc90350057d445c37cc9bd0e4dfe9ee85ee7
issacleral <rodrigosalvatierracalla@gmail.com>
2025-03-12 19:30:00 +0100

    Agregar_contenido

---
+++ Examen.py
@@ -0,0 +1,15 @@
+#Gabriel Rodrigo Salvatierra
+
+def buscarPalabra(objetivo, palabras):
+
+
+    def imprimirListaInversa(lista):
+
+
+    nombres = ["Mengano", "Fulano", "Zutano", "Perantano"]
+    edades = {
+    "Mengano": 0,
+    "Fulano": 25,
+    "Zutano": 50,
+    "Perantano": 75
+        }
